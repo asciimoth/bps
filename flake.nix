14292d91cca47b7cc37c4f6406974397a285f01b
AsciiMoth <ascii@moth.contact>
2025-03-21 11:05:54 +0400

    Setup nix dev env

--- flake.nix
+++ flake.nix
@@ -1,10 +1,36 @@
 {
   description = "Ban stickers on telegram";
-
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
-
-  outputs = { self, nixpkgs }:
-    {
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    pre-commit-hooks,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      checks = {
+        pre-commit-check = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+          };
+        };
+      };
+    in {
       nixosModule = import ./module.nix;
-    };
+      devShell = pkgs.mkShell {
+        inherit (checks.pre-commit-check) shellHook;
+        buildInputs = [
+          pkgs.cargo
+          pkgs.rustfmt
+          pkgs.rust-analyzer
+          pkgs.rustup
+        ];
+      };
+    });
 }
